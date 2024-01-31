3282d6b26aced3154f4ea690e23ac616b2e08733
paulrwade <pau2237029@maricopa.edu>
2024-01-30 17:44:57 -0700

    Initial check in

---
+++ 1_About_Me/about_me.py
@@ -0,0 +1,7 @@
+# Paul Wade
+# PA1
+
+print("My name is Paul Wade")
+print("My favorite song is Turn by The Wombats")
+print("My favorite movie is Midsommar starring Florence Pugh")
+print("My favorite food is carne asada burritos")
