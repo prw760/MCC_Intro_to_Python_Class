b9be3c38dd8c2e390e427c4339c4f0a44b10de1f
paulrwade <pau2237029@maricopa.edu>
2024-03-12 18:03:23 -0700

    Initial check in of exercise assignment 5

---
+++ 11_Exercise_Assignment_5/exercise_assignment_5.py
@@ -0,0 +1,49 @@
+# EA 5
+# Paul Wade
+
+# CHAPTER 8
+# 1. Assign any whole number to variable var_1 (1 pt.).
+var_1 = 8
+
+
+# 2. Write an if statement that assigns 10 to the variable var_2 and -5 to the variable var_3
+# and prints the sum of var_2 and var_3, if the variable var_1 is less than or equal to 10 (3 pts.).
+
+if var_1 <= 10:
+
+	var_2 = 10
+	var_3 = -5
+
+	print(var_2 + var_3)
+
+
+# 3. Assign any whole number to var_4 (1 pt.).
+var_4 = 24
+
+
+
+# 4. Write an if-else statement that prints out 'Yes you can', if var_4 is greater than 20 and
+# prints out 'No you can not' if it is not greater than 20 (3 pts.).
+# 5. Assign any positive whole number to pulse (1 pt.).
+pulse =
+
+
+
+
+# 6. Write an if-else statement that prints 'Your pulse is normal' if pulse
+# is within the range of 60 to 100, inclusively. If pulse is outside the range,
+# print 'Your pulse is not normal' (4 pts.).
+
+
+
+# 7. Use the try except keywords to verify that a user has entered a valid int value by
+# prompting the user to enter an integer. If the value is a valid integer, print out 'You entered a valid integer'.
+# If the value is not a valid integer print out 'That is not a valid integer' (3 pts.).
+
+
+
+# 8. Import the random module and generate two random integer numbers. The first random number
+# should be between -25 and -60 and the second random number should be between 25 and 75.
+# Print both random numbers (4 pts.).
+
+
