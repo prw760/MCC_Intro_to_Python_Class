bcd79340123f5d8eec4dff700eae47099e7160bd
paulrwade <pau2237029@maricopa.edu>
2024-03-29 12:55:45 -0700

    Initial check in of exercise_assignment_6

---
+++ 14_Exercise_Assignment_6/exercise_assignment_6.py
@@ -0,0 +1,53 @@
+# EA 6
+# Paul Wade
+
+# CHAPTER 9
+
+# 1. Create a tuple named integer_tuple that contains the even integer values between -20 and -10.
+# The tuple should start with -20 and end with -10 (3 pts.).
+
+
+# 2. Use indexing to print the value -12 (1 pt.).
+
+
+# 3. Use slicing to print (-14, -12, -10) (2 pts.).
+
+
+# 4. Iterate over the integer_tuple using a for in loop to display each integer in the tuple on a separate line (2 pts.).
+
+
+# 5. Create a list named fruit_list that contains Strawberry and Banana, print the list (2 pts).
+
+
+# 6. Add Grapes to the list using the append function, print the list (2 pts.).
+
+
+# 7. Insert Watermelon into the list at index 2 ( 1 pt.).
+
+
+# 8. Write an if statement using the keyword in to determine if Apple is in the list.  If Apple is in the list,
+# print out: Apple is in the list. If Apple is not in the list print out: Apple is not in the list (2 pts.).
+
+
+# 9. Use a for in loop to print the fruits in the list, each fruit should be printed on the same line with a space after each fruit (2 pts.).
+
+
+# 10. Use the extend function to add Orange, Kiwi and Mango to the list, print the list (2 pts.).
+print() # Do not delete this line
+
+
+# 11. Replace Banana with Plantain in the list using indexing (1 pt.).
+
+
+# 12. Remove Grapes from the list using the pop function, print the list (2 pts.).
+
+
+# 13. Use slicing to print the names Plantain, Watermelon, Orange (1 pt.).
+
+
+# 14. Create a list named integer_list that contains the odd integers values -25 through -11 using
+# a list comprehension and the step value in the range.  The list should start with -25
+# and end with -11. Print the list (2 pts.).
+
+
+
