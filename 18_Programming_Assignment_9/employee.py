0d0326293b81b75ffc58cc2208f9478803020116
paulrwade <pau2237029@maricopa.edu>
2024-04-10 15:48:49 -0700

    check-in of completed programming assignment 9

--- 18_Programming_Assignment_9/employee.py
+++ 18_Programming_Assignment_9/employee.py
@@ -1,3 +1,38 @@
 # Paul Wade
 # PA 9
 
+class Employee:
+	def __init__(self, empID, name, department, yearly_salary):
+		self.employee_number = empID
+		self.name = name
+		self.department = department
+		self.yearly_salary = yearly_salary
+
+	def __str__(self):
+		return (f"\nEmployee Information: \
+				\nEmployee Number: {self.employee_number} \
+				\nName: {self.name} \
+				\nDepartment: {self.department} \
+				\nYearly Salary: ${self.yearly_salary:,}")
+
+	def update_department(self, department):
+		self.department = department
+
+	def update_salary(self, yearly_salary):
+		self.yearly_salary = yearly_salary
+
+
+print("Enter the employee information:")
+employee_number = int(input("Enter employee number: "))
+employee_name = str(input("Enter employee name: "))
+employee_department = str(input("Enter employee department: "))
+employee_salary = int(input("Enter employee salary: "))
+
+myEmployee = Employee(employee_number, employee_name, employee_department, employee_salary)
+
+print(myEmployee)
+
+myEmployee.update_department("Finance")
+myEmployee.update_salary(75000)
+
+print(myEmployee)
