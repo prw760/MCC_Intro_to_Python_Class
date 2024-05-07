b826f04ce0ecb4e4923a210f09389875b6488556
paulrwade <pau2237029@maricopa.edu>
2024-05-06 18:01:37 -0700

    mid-stream check-in of Programming Assignment 15

--- 26_Programming_Assignment_15/sum_client.py
+++ 26_Programming_Assignment_15/sum_client.py
@@ -4,4 +4,10 @@
 import socket
 
 
+# The server program should include a function named sum_random_numbers. The function should generate three random
+# integers between -100 and 100, and sum the absolute values of num1, num2, and num3. The function should return the
+# following information: num1 + num2 + num3 = sum
+
 def server_program():
+	host = socket.gethostname()
+	port = 9876
