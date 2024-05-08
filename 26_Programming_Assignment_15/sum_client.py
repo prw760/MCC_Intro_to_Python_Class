399862bcb93d76f08ff02ebbad3f4a78516c49f6
paulrwade <pau2237029@maricopa.edu>
2024-05-08 12:55:30 -0700

    Final Check In of Reworked (from Scratch) Programming Assignment 15

--- 26_Programming_Assignment_15/sum_client.py
+++ 26_Programming_Assignment_15/sum_client.py
@@ -3,22 +3,13 @@
 import socket
 
 
+# connect to the server, receive random-number-summing information, and print it
 def client_program():
 
-	host = socket.gethostname()
-	port = 9876
+	host = socket.gethostname()         # as both code is running on same pc
+	port = 9876                         # socket server port number
 
-	client_socket = socket.socket()
-	client_socket.connect((host, port))
+	client_socket = socket.socket() 	# instantiate
+	client_socket.connect((host, port)) # connect to the server
 
-	data = client_socket.recv(1024).decode()
-
-	print("Information received from server:")
-	print(data)
-
-	client_socket.close()  # close the connection
-	print("Connection to server closed")
-
-
-if __name__ == '__main__':
-	client_program()
+	data = client_s
--- 26_Programming_Assignment_15/sum_server.py
+++ 26_Programming_Assignment_15/sum_server.py
@@ -4,48 +4,27 @@ import socket
 import random
 
 
+# generate 3 random numbers, sum them, and return the sum as a string
 def sum_3_random_numbers():
 
 	random_1 = abs(random.randint(-100, 100))
 	random_2 = abs(random.randint(-100, 100))
 	random_3 = abs(random.randint(-100, 100))
 
-	total = random_1 + random_2 + random_3
+	total = random_1 + random_2 + random_3  # sum the 3 random numbers
 
+	# create a string with the 3 random numbers, the sum, and an equals sign
 	return_string = (str(random_1) + " + " + str(random_2) + " + " + str(random_3) + " = " + str(total))
 
-	return return_string
+	return return_string   # return the string
 
 
+# create a server that sends the sum of 3 random numbers to a client
 def server_program():
 
-	host = socket.gethostname()
-	port = 9876
+	host = socket.gethostname()     # get the hostname
+	port = 9876                     # initiate port no above 1024
 
-	server_socket = socket.socket()
+	server_socket = socket.socket()     # get instance
 
-	server_socket.bind((host, port))
-
-	while True:
-
-		server_socket.listen(2)
-
-		while True:
-
-			print("Waiting for connection...")
-
-			conn, address = server_socket.accept()
-
-			print("...connected from: " + str(address))
-
-			data = sum_3_random_numbers()
-
-			conn.send(data.encode())
-			print("Response sent to client")
-
-			conn.close()
-			print("Client connection closed\n")
-
-
-if __name__ == '__main__':
-	server_program()
+	server_socket.bind((host, port))    # bind host address and port t
